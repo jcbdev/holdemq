3d51724de80cd3fa6199d3fc660f0fc704283880
jcbdev <james@jcbdevelopment.co.uk>
2018-01-06 20:18:12 +0000

    Changed first two layers to dense

--- play.py
+++ play.py
@@ -9,26 +9,20 @@ seats = 10
 grid_size = 66
 hidden_size = 32
 nb_frames = 5
-lstm_size = 64
+lstm_size = 16
 batch_size = 5000
+learning_rate = 0.00001
 
 model = Sequential()
-# model.add(Convolution1D(32, 3, activation='relu', input_shape=(nb_frames, grid_size)))
-# model.add(Convolution1D(32, 3, activation='relu'))
-# model.add(Convolution1D(16, 3, activation='relu'))
-# model.add(Flatten())
-model.add(LSTM(lstm_size, return_sequences=True, input_shape=(nb_frames, grid_size)))
-model.add(Dropout(0.2))
-model.add(LSTM(int(lstm_size / 2), return_sequences=True))
-model.add(Dropout(0.2))
-model.add(LSTM(int(lstm_size / 4)))
-# model.add(Dense(int(hidden_size), activation='relu', input_shape=(nb_frames, grid_size)))
+# model.add(Convolution1D(int(hidden_size), 3, activation='relu', input_shape=(nb_frames, grid_size)))
 # model.add(Dropout(0.2))
-# model.add(Dense(int(hidden_size/2), activation='relu'))
-# model.add(Dropout(0.2))
-# model.add(Dense(int(hidden_size/4), activation='relu'))
-# model.add(Dense(6))
-model.compile(RMSprop(), 'MSE')
+model.add(Dense(int(hidden_size), activation='relu', input_shape=(nb_frames, grid_size)))
+model.add(Dropout(0.2))
+model.add(Dense(int(hidden_size/2), activation='relu'))
+model.add(Dropout(0.2))
+model.add(LSTM(int(lstm_size)))
+
+model.compile(Adam(lr=learning_rate), 'MSE')
 model._make_predict_function()
 
 # controller for human meat bag
@@ -38,7 +32,6 @@ trainers = []
 for i in range(1, seats + 1):
     trainers.append(Train())
 
-epoch = 0
 while True:
     t = Table(seats, training=True, quiet=True)
     print('starting ai players')
@@ -46,4 +39,3 @@ while True:
     for i in range(1, seats + 1):
         p = PlayerControl(i, t, model=model, train=trainers[i-1])
     t.run_game()
-    epoch += 1
--- slackbot/plugins/playpoker.py
+++ slackbot/plugins/playpoker.py
@@ -8,8 +8,14 @@ class PlayPokerPlugin(Plugin):
     def process_message(self, data):
         if data['channel'].startswith("D"):
             print(data)
+            #json = json.loads(data)
+            if data.type == "message" and data.text == "Play a game!":
+                self.start_a_game(data)
             # self.outputs.append(
             #     [data['channel'], 'from repeat1 "{}" in channel {}'.format(
             #         data['text'], data['channel']
             #     )]
             # )
+
+    def start_a_game(self, data):
+        print ("Starting a new game")
